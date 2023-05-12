1e69e3a191b8edb978c1ecff46fa0c98acdbc577
PyroThree <benjituan@gmail.com>
2023-05-12 11:11:06 -0500

    added menu button from shop and made it so that shop buttons aren't visible when gui launches

--- calculator.py
+++ calculator.py
@@ -10,6 +10,24 @@ class Calculator(QMainWindow, Ui_MainWindow):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
+        self.label_takoyaki.setVisible(False)
+        self.label_ramune.setVisible(False)
+        self.label_ramen.setVisible(False)
+        self.input_takoyaki.setVisible(False)
+        self.input_ramune.setVisible(False)
+        self.input_ramen.setVisible(False)
+        self.button_add_takoyaki.setVisible(False)
+        self.button_add_ramune.setVisible(False)
+        self.button_add_ramen.setVisible(False)
+        self.button_clear.setVisible(False)
+        self.button_main.setVisible(False)
+        self.label_total.setVisible(False)
+        self.button_clear_takoyaki.setVisible(False)
+        self.button_clear_ramune.setVisible(False)
+        self.button_clear_ramen.setVisible(False)
+
+        self.button_shop
+
     def add(self):
         pass
 
